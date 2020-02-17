0d57d860270f548838ce499eee2c6eb5f510537d
keerthana703 <61132943+keerthana703@users.noreply.github.com>
2020-02-17 11:46:00 +0530

    Add files via upload

---
+++ p4..py
@@ -0,0 +1,14 @@
+import random;
+a=random.randint(1,6)
+def rans():
+    i=1
+    while(1<10):
+        b=int(input("Enter any number"));
+        if a==b:
+            print("you win",a,'=',b);
+        else:
+            print("you lose",a,'not equal to',b);
+            print("Try again...!")
+            break
+    i=i+1;
+rans();
